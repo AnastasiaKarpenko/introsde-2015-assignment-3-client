92add94196b2f5dc24f18298302ffbe3c4668e30
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2015-12-07 20:49:06 +0100

    Added a logfile.txt

--- src/client/PeopleClient.java
+++ src/client/PeopleClient.java
@@ -1,5 +1,7 @@
 package client;
 
+import java.io.PrintWriter;
+
 import java.util.List;
 import java.util.LinkedList;
 
@@ -11,31 +13,34 @@ import introsde.assignment.soap.ws.*;
 
 
 public class PeopleClient{
+	private static PrintWriter logWriter;
+
     public static void main(String[] args) throws Exception {
         PeopleService service = new PeopleService();
         People people = service.getPeopleImplPort();
+        logWriter = new PrintWriter("logfile.txt", "UTF-8");
         Person p = people.readPerson(1);
 	    
         
         
         //Method #1 Print all the people 
-        System.out.println("  ");
-        System.out.println("METHOD #1: PRINTING ALL THE PEOPLE ");
+        logWriter.println("  ");
+        logWriter.println("METHOD #1: PRINTING ALL THE PEOPLE ");
         List<Person> pList = people.getPeopleList();
         for (Person per : pList) {
-        	System.out.println("   ");
+        	logWriter.println("   ");
         	printPersonDetails(per);
         	List<LifeStatus> lsList = per.getLifeStatus();
         	for (LifeStatus ls : lsList) {
         		printLifeStatus(ls);
         	}
         }
-        System.out.println("  ");
-        System.out.println("People number: "+pList.size());
+        logWriter.println("  ");
+        logWriter.println("People number: "+pList.size());
 
        //Method #2 Print a person (with id=1)
-        System.out.println("   ");
-        System.out.println("METHOD #2: READING THE PERSON WITH ID = 1 ");
+        logWriter.println("   ");
+        logWriter.println("METHOD #2: READING THE PERSON WITH ID = 1 ");
         printPersonDetails(p);
 
         List<LifeStatus> lsList = p.getLifeStatus();
@@ -50,8 +55,8 @@ public class PeopleClient{
     	people.updatePerson(newPerson);
 
     	newPerson = people.readPerson(1);
-    	System.out.println("   ");
-        System.out.println("METHOD #3: UPDATING PERSON WITH ID = 1 (the first name only) ");
+    	logWriter.println("   ");
+        logWriter.println("METHOD #3: UPDATING PERSON WITH ID = 1 (the first name only) ");
     	printPersonDetails(newPerson);
 
     	
@@ -78,44 +83,44 @@ public class PeopleClient{
     	int newId = people.createPerson(createChuckNorris);
 
     	createChuckNorris = people.readPerson(newId);
-    	System.out.println("   ");
-        System.out.println("METHOD #4: CREATING A NEW PERSON ");
+    	logWriter.println("   ");
+        logWriter.println("METHOD #4: CREATING A NEW PERSON ");
     	printPersonDetails(createChuckNorris);
     	List<LifeStatus> lsList2 = createChuckNorris.getLifeStatus();
         	for (LifeStatus ls2 : lsList2) {
         		printLifeStatus(ls2);
         	}
 
-    	System.out.println("  ");
+    	logWriter.println("  ");
     	pList = people.getPeopleList();
-		System.out.println("People number before deleting the previousely created person: "+pList.size());
+		logWriter.println("People number before deleting the previousely created person: "+pList.size());
 
 
     	//Method #5 Deleting a person
     	
 		people.deletePerson(newId);
-		System.out.println("   ");
-		System.out.println("METHOD #5: DELETING A PREVIOUSELY CREATED PERSON ");
+		logWriter.println("   ");
+		logWriter.println("METHOD #5: DELETING A PREVIOUSELY CREATED PERSON ");
 		pList = people.getPeopleList();
-		System.out.println("People number after deleting the previousely created person: "+pList.size());
+		logWriter.println("People number after deleting the previousely created person: "+pList.size());
 		
 		
 		//Method #6 Read person's measure History of one measure
-		System.out.println("  ");
-        System.out.println("METHOD #6: READING AND PRINTING HISTORY OF WEIGHT MEASURES FOR PERSON ID = 1");
+		logWriter.println("  ");
+        logWriter.println("METHOD #6: READING AND PRINTING HISTORY OF WEIGHT MEASURES FOR PERSON ID = 1");
         List<HealthMeasureHistory> hList = people.readPersonHistory(1, "weight");
         for (HealthMeasureHistory hist : hList) {
-        	System.out.println("   ");
+        	logWriter.println("   ");
         	printMeasureHistory(hist);
         	
         }
 	
         //Method #7 Read all measure types
-		System.out.println("  ");
-        System.out.println("METHOD #7: READING AND PRINTING ALL MEASURE TYPES");
+		logWriter.println("  ");
+        logWriter.println("METHOD #7: READING AND PRINTING ALL MEASURE TYPES");
         List<MeasureDefinition> mhList = people.readMeasureTypes();
         for (MeasureDefinition mh : mhList) {
-        	System.out.println("   ");
+        	logWriter.println("   ");
         	printAllMeasureTypes(mh);
         	
         }
@@ -123,14 +128,14 @@ public class PeopleClient{
         // Method #8 Read value of measure type mid=1 
 
         HealthMeasureHistory hmh = people.readMeasureHistoryId(1);
-		System.out.println("  ");
-        System.out.println("METHOD #8: PRINTING THE VALUE OF A MEASURE TYPE WITH MID ");
-        System.out.println("Weight with mid = 1 of the person id = 1 has value: " + hmh.getValue());
+		logWriter.println("  ");
+        logWriter.println("METHOD #8: PRINTING THE VALUE OF A MEASURE TYPE WITH MID ");
+        logWriter.println("Weight with mid = 1 of the person id = 1 has value: " + hmh.getValue());
 
 
 		// Method #9 create a new measure 
-        System.out.println("  ");
-        System.out.println("METHOD #9: CREATE A NEW MEASURE (WEIGHT) AND UPDATE A HEALTH MEASURE HISTORY OF PERSON 1");
+        logWriter.println("  ");
+        logWriter.println("METHOD #9: CREATE A NEW MEASURE (WEIGHT) AND UPDATE A HEALTH MEASURE HISTORY OF PERSON 1");
     	
     	HealthMeasureHistory newHMH = new HealthMeasureHistory();
     	newHMH.setValue("99");
@@ -143,43 +148,45 @@ public class PeopleClient{
         	printLifeStatus(ls3);
         }
 
-        System.out.println("  ");
-        System.out.println( "READING AND PRINTING HISTORY OF WEIGHT MEASURES FOR PERSON ID = 1");
+        logWriter.println("  ");
+        logWriter.println( "READING AND PRINTING HISTORY OF WEIGHT MEASURES FOR PERSON ID = 1");
         List<HealthMeasureHistory> hList2 = people.readPersonHistory(1, "weight");
         for (HealthMeasureHistory hist2 : hList2) {
-        	System.out.println("   ");
+        	logWriter.println("   ");
         	printMeasureHistory(hist2);
         	
         }
+
+        logWriter.close();
     	
     }
 
 
     public static void printLifeStatus(LifeStatus ls) {
-    	System.out.println("Person's lifestatus measure: ");
-    	System.out.println("Measure name: " + ls.getMeasure());
-    	System.out.println("Value: " + ls.getValue());
+    	logWriter.println("Person's lifestatus measure: ");
+    	logWriter.println("Measure name: " + ls.getMeasure());
+    	logWriter.println("Value: " + ls.getValue());
 
     }
 
     public static void printPersonDetails(Person per) {
-    	System.out.println("Person id: " + per.getIdPerson());
-        System.out.println("Person's name: " + per.getName());
-        System.out.println("Person's lastname: " + per.getLastname());
-        System.out.println("Person's birthdate: " + per.getBirthdate());
+    	logWriter.println("Person id: " + per.getIdPerson());
+        logWriter.println("Person's name: " + per.getName());
+        logWriter.println("Person's lastname: " + per.getLastname());
+        logWriter.println("Person's birthdate: " + per.getBirthdate());
         
     }
 
     public static void printMeasureHistory(HealthMeasureHistory hist) {
-    	System.out.println("Id of Measure History " + hist.getIdMeasureHistory());
-        System.out.println("Measure name: " + hist.getMeasureName());
-        System.out.println("Timestamp of the measure: " + hist.getTimestamp());
-        System.out.println("Measure's value: " + hist.getValue());
+    	logWriter.println("Id of Measure History " + hist.getIdMeasureHistory());
+        logWriter.println("Measure name: " + hist.getMeasureName());
+        logWriter.println("Timestamp of the measure: " + hist.getTimestamp());
+        logWriter.println("Measure's value: " + hist.getValue());
         
     }
 
     public static void printAllMeasureTypes(MeasureDefinition mh) {
-    	System.out.println("Measure type: " + mh.getMeasureName());
+    	logWriter.println("Measure type: " + mh.getMeasureName());
         
     }
 
